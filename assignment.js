2c7ee555a4b956b7b02f62ccd2cd371515c86387
kapooraayusi <114466645+kapooraayusi@users.noreply.github.com>
2022-09-29 00:02:29 +0530

    Add files via upload
    Assignment1 - Variables,Constants,Operators and Core Data Types

---
+++ assignment.js
@@ -0,0 +1,10 @@
+let userInput = 10;
+let result;
+
+result = 18 + userInput;
+result = result - 1;
+result = result * 10;
+result = result / 2;
+
+alert("User Input: " + userInput);
+alert("Result: " + result);
